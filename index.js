0684529d643f144c9c942536ea2b01ea59eff06f
Joel Jaison <joeljaison394@gmail.com>
2023-09-18 21:28:09 +0530

    test files removed

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import { startServer } from './server.js';
 const server = startServer();
 
 process.on('unhandledRejection', (error) => {
-  logger.error('Unhandled Promise Rejection:', error);
+
 });
 
 process.on('uncaughtException', (error) => {
-  logger.error('Uncaught Exception:', error);
+
   server.close(() => {
     process.exit(1);
   });
